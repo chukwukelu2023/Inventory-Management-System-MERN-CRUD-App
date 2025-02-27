ace0f200cf7b93f18153dbcf80aecfcac9739a73
chukwukelu2023 <chukwukeluoo@gmail.com>
2025-02-27 02:32:28 +0100

    added docker and docker-compose files

--- Backend/db.js
+++ Backend/db.js
@@ -4,7 +4,7 @@ const mongoURI = process.env.mongoURI;
 const connectToMongo = async () => {
   try {
     mongoose.set("strictQuery", false);
-    mongoose.connect(mongoURI);
+    await mongoose.connect(mongoURI);
     console.log("Connected to Mongo Successfully!");
   } catch (error) {
     console.log(error);
