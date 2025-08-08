60bf2673e108d534ac5d8b0e34f7198fb1441bbe
chukwukelu2023 <chukwukeluoo@gmail.com>
2025-08-08 16:37:38 +0100

    added console.log

--- Backend/db.js
+++ Backend/db.js
@@ -3,11 +3,12 @@ const mongoURI = process.env.mongoURI;
 
 const connectToMongo = async () => {
   try {
+    console.log({mongoURI})
     mongoose.set("strictQuery", false);
     await mongoose.connect(mongoURI);
     console.log("Connected to Mongo Successfully!");
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
   }
 };
 module.exports = connectToMongo;
